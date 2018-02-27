d6ceedac81ca3cf13b233f2a135266995e73b4fa
Alaric Snell-Pym <alaric@dotmesh.io>
2018-02-27 13:52:15 +0000

    Re-enable docker4mac!

--- build_kernel_modules.sh
+++ build_kernel_modules.sh
@@ -32,15 +32,15 @@ function build {
 
 # docker4mac
 
-#versions=$(cd d4m-poller && ./check.sh)
-#echo Building docker4mac versions:
-#echo $versions
-#for version in $versions; do
-#    build ${version} ${version}-linuxkit-aufs linuxkit
-#    # try to be forward compatible for when versions without aufs patches
-#    # become edge/stable
-#    build ${version} ${version}-linuxkit linuxkit
-#done
+versions=$(cd d4m-poller && ./check.sh)
+echo Building docker4mac versions:
+echo $versions
+for version in $versions; do
+    build ${version} ${version}-linuxkit-aufs linuxkit
+    # try to be forward compatible for when versions without aufs patches
+    # become edge/stable
+    build ${version} ${version}-linuxkit linuxkit
+done
 
 # boot2docker
 # look up docker version -> kernel mapping here:
