6aa2cd7dca91f6984e01f083ca1016ccb3f847f6
Luke Marsden <me@lukemarsden.net>
2018-03-02 08:26:03 +0000

    behaves differently in a container, this dir does not show up.

--- build_zfs.centos-7-4.sh
+++ build_zfs.centos-7-4.sh
@@ -10,10 +10,8 @@ rm -rf *
 VERSION=`rpm -ql kmod-zfs | head -1 | sed s_/lib/modules/__ | sed 's_/.*$__'`
 
 tar cf - `rpm -ql kmod-zfs` `rpm -ql kmod-spl` | tar xf -
-for VERS in `cd /lib/modules ; ls | grep -v $VERSION`
-do
-	 ln -s $VERSION lib/modules/$VERS
-done
+
+ln -s $VERSION lib/modules/3.10.0-693.11.6.el7.x86_64
 
 tar czfv /rootfs/zfs-${UNAME_R}.tar.gz lib
 echo "Plopped it into /rootfs/zfs-${UNAME_R}.tar.gz"
