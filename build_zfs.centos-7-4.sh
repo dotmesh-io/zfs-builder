a8a986f83b3890e8d27beb723601dc24bab0b820
Luke Marsden <me@lukemarsden.net>
2018-03-02 09:12:57 +0000

    Also link in the latest kernel that RHEL 7.4 tries to install when you 'yum update' on the AWS images.

--- build_zfs.centos-7-4.sh
+++ build_zfs.centos-7-4.sh
@@ -12,6 +12,7 @@ VERSION=`rpm -ql kmod-zfs | head -1 | sed s_/lib/modules/__ | sed 's_/.*$__'`
 tar cf - `rpm -ql kmod-zfs` `rpm -ql kmod-spl` | tar xf -
 
 ln -s $VERSION lib/modules/3.10.0-693.11.6.el7.x86_64
+ln -s $VERSION lib/modules/3.10.0-693.17.1.el7.x86_64
 
 tar czfv /rootfs/zfs-${UNAME_R}.tar.gz lib
 echo "Plopped it into /rootfs/zfs-${UNAME_R}.tar.gz"
