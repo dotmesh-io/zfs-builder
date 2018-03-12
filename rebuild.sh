a617782acb1f36c7f021b25299bdcd6b1563c870
Alaric Snell-Pym <alaric@dotmesh.io>
2018-03-12 16:31:57 +0000

    dotmesh#308: CronJob confirmed working, but needs k8s 1.8+ to work.
    This commit also comments out the "dogfooding" of storing the cache in a dot
    but instead puts them in /tmp/zfs-kernel-modules

--- rebuild.sh
+++ rebuild.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 docker build -t zfs-builder .
+
+docker tag zfs-builder zfs-builder:dev
+docker tag zfs-builder quay.io/dotmesh/zfs-builder:dev
+docker push quay.io/dotmesh/zfs-builder:dev
