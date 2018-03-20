cb6ec5a0831984de15a139c7bb6f6207d5e47469
Alaric Snell-Pym <alaric@dotmesh.io>
2018-03-20 10:28:09 +0000

    dotmesh#331: Fixes to track changed third-party stuff

--- d4m-poller/check.sh
+++ d4m-poller/check.sh
@@ -5,7 +5,8 @@ for X in stable edge; do
     cd tmp
     curl -sSL -o Docker.dmg https://download.docker.com/mac/$X/Docker.dmg
     7z x Docker.dmg >/dev/null
-    strings 4.hfs |grep "#1 SMP" |awk '{print $1}' |awk -F "-" '{print $1}'
+    FILE_WITH_LINUX="`find Docker -type f -exec grep -q '#1 SMP' {} \; -print`"
+    strings "$FILE_WITH_LINUX" |grep "#1 SMP" |awk '{print $1}' |awk -F "-" '{print $1}'
     cd ..
     rm -rf tmp
 done
